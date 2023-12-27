bf56eb5a9ff75f88bcec25212da0955b0dcf2730
Lumin228 <6786679@gmail.com>
2023-12-27 11:05:39 +0200

    ss

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const dropdownMenu = document.querySelector('#dropdownMenu');
+const inputText = document.querySelector('#input-filter')
+const buttonForm = document.querySelector('#filterButt')
+const LOCAL_SAVE_INPUT = "keyword";
+const LOCAL_SAVE_CATEGORY = "category";
 
-var data = ['Option 1', 'Option 2', 'Option 3', 'Option 3'];
+inputText.addEventListener('input', inputPush)
+buttonForm.addEventListener('click', cardsCreate)
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const data = ['Option 1', 'Option 2', 'Option 3', 'Option 3'];
 
 
 function createDropdownList() {
-    const dropdownMenu = document.getElementById('dropdownMenu');
-    
-   
+
     dropdownMenu.innerHTML = '';
 
    
@@ -15,22 +23,43 @@ function createDropdownList() {
         listItem.textContent = item;
         listItem.onclick = function () {
            
-            console.log('Selected:', item);
+            itemCategory = item;
+            localStorage.setItem(LOCAL_SAVE_CATEGORY, JSON.stringify(itemCategory));
             toggleDropdown();
         };
         dropdownMenu.appendChild(listItem);
     });
 }
 
-
 createDropdownList();
 
-
 function toggleDropdown() {
-    const dropdownMenu = document.getElementById('dropdownMenu');
+
     dropdownMenu.style.display = (dropdownMenu.style.display === 'flex') ? 'none' : 'flex';
 
     setTimeout(function () {
         dropdownMenu.style.opacity = (dropdownMenu.style.opacity === '1') ? '0' : '1';
     }, 10);
+}
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+let inputTextHolder = '';
+let itemCategory = '';
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function inputPush(e) {
+    const value = e.target.value; 
+    inputTextHolder = value;
+    localStorage.setItem(LOCAL_SAVE_INPUT, JSON.stringify(inputTextHolder));
+}
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function cardsCreate (event) {
+    event.preventDefault();
+    console.log(inputTextHolder)
+    console.log(itemCategory)
+    
 }
